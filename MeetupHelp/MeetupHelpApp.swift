74252d843c9c7787197ecb3d3c328fa27947ccd9
Leo Barnett <leo.e.barnett@icloud.com>
2024-01-25 13:18:48 +0200

    Initial Commit

---
+++ MeetupHelp/MeetupHelpApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeetupHelpApp.swift
+//  MeetupHelp
+//
+//  Created by Leo  on 25.01.24.
+//
+
+import SwiftUI
+
+@main
+struct MeetupHelpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
